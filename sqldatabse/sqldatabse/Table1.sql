2a59ace2cf31e8ca1fd10b7cb6e9b6306cce291c
myfirstgithubacc <snjkmr460@gmail.com>
2022-09-19 15:01:25 +0530

    new db with no change

---
+++ sqldatabse/sqldatabse/Table1.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table1]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [abc] NCHAR(10) NULL, 
+    [xyz] NCHAR(10) NULL, 
+    [mno] NCHAR(10) NULL
+)
---
+++ sqldatabse/sqldatabse/Table2.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table2]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [ab] NCHAR(10) NULL, 
+    [cd] NCHAR(10) NULL, 
+    [ef] NCHAR(10) NULL
+)
