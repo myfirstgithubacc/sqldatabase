ea95fdd23278929a250b138826612a96e91ca7f5
myfirstgithubacc <snjkmr460@gmail.com>
2022-09-19 15:33:29 +0530

    change in table "not null"

--- sqldatabse/sqldatabse/Table1.sql
+++ sqldatabse/sqldatabse/Table1.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Table1]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [abc] NCHAR(10) NULL, 
-    [xyz] NCHAR(10) NULL, 
-    [mno] NCHAR(10) NULL
+    [abc] NCHAR(10) NOT NULL, 
+    [xyz] NCHAR(10) NOT NULL, 
+    [mno] NCHAR(10) NOT NULL
 )
--- sqldatabse/sqldatabse/Table2.sql
+++ sqldatabse/sqldatabse/Table2.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Table2]
 (
 	[Id] INT NOT NULL PRIMARY KEY, 
-    [ab] NCHAR(10) NULL, 
-    [cd] NCHAR(10) NULL, 
-    [ef] NCHAR(10) NULL
+    [ab] NCHAR(10) NOT NULL, 
+    [cd] NCHAR(10) NOT NULL, 
+    [ef] NCHAR(10) NOT NULL
 )
